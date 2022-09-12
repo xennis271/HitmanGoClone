22a5fd126107afd4d8291aa4af0e30a1a3269218
xennis271 <gxennis20@gmail.com>
2022-09-12 08:34:02 -0500

    UI-Slide
    Just did the title for now but plan on making the names and jobs to

---
+++ Assets/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text SlidingText;
+    public Vector3 target;
+    
+    float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Starting UI animation
+        print("[UIManager]: SlideOnScreen Starting");
+        Vector3 Offset = new Vector3(SlidingText.rectTransform.position.x - 500, SlidingText.rectTransform.position.y, SlidingText.rectTransform.position.z);
+        target = SlidingText.rectTransform.position;
+        SlidingText.rectTransform.position = Offset;
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // X - 500
+        t += Time.deltaTime / 5.0f;
+        
+        
+        SlidingText.rectTransform.position = Vector3.Lerp(SlidingText.rectTransform.position, target, t);
+    }
+}
