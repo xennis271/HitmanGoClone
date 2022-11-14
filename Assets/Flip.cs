45ab2cbca00071b8a28af72efe6f84caef17001e
xennis271 <gxennis20@gmail.com>
2022-11-14 02:00:13 -0600

    Movement!
    We have basic player movement with no restrictions

---
+++ Assets/Flip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flip : MonoBehaviour
+{
+    public GameObject Icon;
+    public Sprite FrameOne;
+    public Sprite FrameTwo;
+    // Start is called before the first frame update
+    public void FlipStart()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Flipe());
+    }
+
+    private IEnumerator Flipe()
+    {
+        WaitForSeconds LoopForMe = new WaitForSeconds(1);
+        while (true)
+        {
+            
+                Icon.GetComponent<SpriteRenderer>().sprite = FrameTwo;
+                //Debug.Log("2");
+            
+            yield return LoopForMe;
+            
+                Icon.GetComponent<SpriteRenderer>().sprite = FrameOne;
+                //Debug.Log("1");
+
+            yield return LoopForMe;
+        }
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject MoveHereIcon;
+    Vector2 lastClickedPoint;
+    bool moving = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClickedPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+            MoveHereIcon.SetActive(true);
+            MoveHereIcon.GetComponent<Flip>().FlipStart();
+        }
+        if (moving && (Vector2)transform.position != lastClickedPoint) // if we are moving and not there
+        {
+            
+            
+            MoveHereIcon.transform.position = lastClickedPoint;
+            MoveHereIcon.transform.position = new Vector3(lastClickedPoint.x, lastClickedPoint.y, -1); // so you can always see it
+            float step = speed * Time.deltaTime; // makes the speed not linked to framerate
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPoint, step);
+        }
+        else
+        {
+            moving = false; // we are here!
+            MoveHereIcon.SetActive(false);
+        }
+    }
+}
