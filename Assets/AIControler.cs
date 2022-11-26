3b816f41cb3626864792e702f0f9018d01d30e08
xennis271 <gxennis20@gmail.com>
2022-11-26 16:56:21 -0600

    Not working view code
    Well I could not get it working but we need to get the view cone to exist when the values are correct and I was struggling getting the arrays to work

--- Assets/AIControler.cs
+++ Assets/AIControler.cs
@@ -6,8 +6,10 @@ public class AIControler : MonoBehaviour
 {
     public GameObject[] BasicAI;
     public Dictionary<GameObject,List<Vector3>> BasicAIMoves = new Dictionary<GameObject,List<Vector3>>();
+    public Dictionary<GameObject, List<string>> BasicAIView = new Dictionary<GameObject, List<string>>();
     public void Start()
     {
+        
         foreach(GameObject AI in BasicAI)
         {
             List<GameObject> AllChildren = new List<GameObject>();
@@ -21,17 +23,50 @@ public class AIControler : MonoBehaviour
             // we have all the kids kick them to get the correct pos in realtion to the world
             AI.transform.DetachChildren();
             List<Vector3> MovePoints = new List<Vector3>();
+            List<string> ViewPoints = new List<string>();
+            
+            foreach (GameObject node in AllChildren)
+            {
+                
+                if(!node.name.Equals("F") && !node.name.Equals("B") && ViewPoints.Count < AllChildren.Count-2)
+                    ViewPoints.Add("");
+                
+            }
+            //Debug.Log("ViewPoint.Count :" + ViewPoints.Count);
             // add self so we have the starting point
             MovePoints.Add(AI.transform.position);
             foreach (GameObject node in AllChildren)
             {
-                //Debug.Log("Node:" + int.Parse(node.name));
-                MovePoints.Insert(int.Parse(node.name)+1, node.transform.position);
-                // remove the node after
-                GameObject.Destroy(node);
+                char[] line = node.name.ToCharArray();
+                if(line.Length != 1)
+                {
+                   int IndexChar = int.Parse(line[1].ToString());
+                   char ViewChar = line[0];
+                   //Debug.Log("Name:" + node.name + " I:" + IndexChar + " V:" + ViewChar);
+                   MovePoints.Insert(IndexChar + 1, node.transform.position);
+                   ViewPoints[IndexChar] = ViewChar.ToString();
+                   // remove the node after
+                   GameObject.Destroy(node);
+                    
+                }
+                else
+                {
+                    node.gameObject.transform.parent = AI.transform;
+                }
+                
             }
+            //Debug.Log("ViewPoint.Count 2:" + ViewPoints.Count);
+            //int count = 0;
+            //foreach (string Line in ViewPoints)
+            //{
+
+            //    Debug.Log(count +  ":" + Line);
+                
+            //    count++;
+            //}
             // put this in the mega list
             BasicAIMoves.Add(AI, MovePoints);
+            BasicAIView.Add(AI, ViewPoints);
             //MovePoints.Clear();
             //AllChildren.Clear();
         }
@@ -45,9 +80,28 @@ public class AIControler : MonoBehaviour
         
         foreach (GameObject AI in BasicAI)
         {
+            //view check
+            int count = 0;
+            foreach (string line in BasicAIView[AI])
+            {
+                Debug.Log(count + ":" + line);
+                if (line == "")
+                    break;
+                count++;
+            }
+            BasicAIView[AI].Capacity = count;
+            count = 0;
+            foreach (string line in BasicAIView[AI])
+            {
+                Debug.Log(count + ":" + line);
+               // if (line == "")
+               //     break;
+                count++;
+            }
+            //MOVEMENT
             // move to the correct spot.
             //Debug.Log(Step+1 + ">" + BasicAIMoves[AI].Count);
-            if(Step >= BasicAIMoves[AI].Count)
+            if (Step >= BasicAIMoves[AI].Count)
             {
                 // fewer steps than the count so just keep a fake step
                 int FakeStep = Step;
@@ -61,14 +115,76 @@ public class AIControler : MonoBehaviour
                 //    FakeStep = 0;
                 //Debug.Log("FakeStep:" + FakeStep);
                 AI.transform.position = BasicAIMoves[AI][FakeStep];
+                //VIEW
+                if (BasicAIView[AI][FakeStep-1] == "F")
+                {
+                    foreach (Transform child in AI.transform)
+                    {
+                        if (child.name.Equals("F"))
+                        {
+                            Debug.Log("SET F true");
+                            child.gameObject.SetActive(true);
+                        }
+                        else
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+                    }
+                }
+                if (BasicAIView[AI][FakeStep-1] == "B")
+                {
+                    foreach (Transform child in AI.transform)
+                    {
+                        if (child.name.Equals("B"))
+                        {
+                            Debug.Log("SET B true");
+                            child.gameObject.SetActive(true);
+                        }
+                        else
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+                    }
+                }
             }
             else
             {
                 Debug.Log("Step:" + Step);
+                //VIEW
+                if (BasicAIView[AI][Step-1] == "F")
+                {
+                    foreach (Transform child in AI.transform)
+                    {
+                        if (child.name.Equals("F"))
+                        {
+                            child.gameObject.SetActive(true);
+                        }
+                        else
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+                    }
+                }
+                if (BasicAIView[AI][Step-1] == "B")
+                {
+                    foreach (Transform child in AI.transform)
+                    {
+                        if (child.name.Equals("B"))
+                        {
+                            child.gameObject.SetActive(true);
+                        }
+                        else
+                        {
+                            child.gameObject.SetActive(false);
+                        }
+                    }
+                }
                 AI.transform.position = BasicAIMoves[AI][Step];
             }
             
         }
+        
+        
         Step++;
     }
 }
